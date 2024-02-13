498899eccb3c537cb517b10a3ffe8d74ecc2ec7e
Roman Syvash <syvashroman0109@gmail.com>
2024-02-13 21:54:23 +0200

    dell all

--- apps/main/urls.py
+++ apps/main/urls.py
@@ -6,5 +6,4 @@ app_name = 'main'
 urlpatterns = [
     path('',views.index, name= 'index'),
     path('magazine/', views.magazine, name = 'magazine'),
-    path('about/', views.about, name = 'about'),
 ]
