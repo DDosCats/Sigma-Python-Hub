49a0473144a160ee193dcecc6f0810715f426a69
Roman Syvash <syvashroman0109@gmail.com>
2024-02-13 22:00:14 +0200

    add about site

--- apps/main/urls.py
+++ apps/main/urls.py
@@ -6,4 +6,5 @@ app_name = 'main'
 urlpatterns = [
     path('',views.index, name= 'index'),
     path('magazine/', views.magazine, name = 'magazine'),
+    path('about/', views.about, name= 'about')
 ]
