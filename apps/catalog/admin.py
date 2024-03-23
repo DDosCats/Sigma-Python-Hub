02b75f820f5300362e8038aefb8152bd8c553c2c
Roman Syvash <syvashroman0109@gmail.com>
2024-03-23 21:58:46 +0200

    nothing

--- apps/catalog/admin.py
+++ apps/catalog/admin.py
@@ -24,8 +24,8 @@ class ImageAdmin(admin.ModelAdmin):
 @admin.register(Catalog)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name', 'image_tag_thumbnail')
-    prepopulated_fields = {'slug': ('name',)}
-    readonly_fields = ('image_tag_thumbnail',) 
+    prepopulated_fields = {'slug': ('name',)} # це поле автоматично заповнюється на основі іншого поля
+    readonly_fields = ('image_tag_thumbnail',) # це поле тільки для читання
     
 
 @admin.register(Product)
